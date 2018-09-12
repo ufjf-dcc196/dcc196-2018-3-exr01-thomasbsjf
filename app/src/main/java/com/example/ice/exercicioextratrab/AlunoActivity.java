ef45b409106d166035a7092b216527a0f8405257
Thomas Santos <thomasbsjf@gmail.com>
2018-09-12 18:53:56 -0300

    Criação dos botões Aluno,Servidor e Externo e criação de suas devidas Activities

---
+++ app/src/main/java/com/example/ice/exercicioextratrab/AlunoActivity.java
@@ -0,0 +1,13 @@
+package com.example.ice.exercicioextratrab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aluno);
+    }
+}
